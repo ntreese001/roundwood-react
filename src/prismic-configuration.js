1f13575de325dc1d1878dacbd13b808072312048
ntreese001 <34658551+ntreese001@users.noreply.github.com>
2021-11-01 15:14:34 -0700

    Configuration File Update

--- src/prismic-configuration.js
+++ src/prismic-configuration.js
@@ -10,12 +10,5 @@ export const apiEndpoint = "https://roundwood.prismic.io/api/v2";
 const accessToken =
   "MC5ZVnRhaUJFQUFDTUF2bmEw.77-9U--_ve-_vU5g77-977-977-9CBE077-9Pe-_vVsJCHPvv73vv73vv71p77-977-9Sk0s77-977-977-977-9";
 
-// -- Link resolution rules
-// Manages the url links to internal Prismic documents
-export const linkResolver = (doc) => {
-  if (doc.type === "page") return `/page/${doc.uid}`;
-  return "/";
-};
-
 // Client method to query documents from the Prismic repo
 export const client = Prismic.client(apiEndpoint, { accessToken });
